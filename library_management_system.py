04c6530ae929dfe051e3c4829646ace0b5a36cf8
Nur Hossain Sakil <hossain.bd1182@gmail.com>
2024-12-16 00:15:52 +0300

    Add data on a json file and prevent duplicates empty

--- library_management_system.py
+++ library_management_system.py
@@ -1,18 +1,52 @@
+import json
+
 class Library():
-    def __init__(self):
+    def __init__(self, filename="library.json"):
         self.books = []
+        self.filename = filename
+        self.load_books()
     
+    def save_book(self):
+        data = [{
+            "title": book.title,
+            "author": book.author,
+            "isbn": book.isbn,
+            "is_borrowed": book.is_borrowed
+        } for book in self.books]
+
+        with open(self.filename, "w") as f:
+            json.dump(data, f, indent = 4)
+
     def add_book(self,book):
+        for existing_book in self.books:
+            if existing_book.isbn == book.isbn:
+                print(f"The book with ISBN: {book.isbn} is already in the Library")
+                return
         self.books.append(book)
+        self.save_book()
         print("Book added Successfully")
 
     def remove_book(self, isbn):
         for book in self.books:
             if book.isbn == isbn:
                 self.books.remove(book)
+                self.save_book()
                 print(f"Removed: {book.title}")
                 return True
         return False
+    
+    def load_books(self):
+        try:
+            with open(self.filename, "r") as file:
+                data = json.load(file)
+                for book_data in data:
+                    book = Book(book_data["title"], book_data["author"], book_data["isbn"])
+                    book.is_borrowed = book_data["is_borrowed"]
+                    self.books.append(book)
+        except FileNotFoundError:
+            print("No previous library data found")
+        except json.JSONDecodeError:
+            print("Error reading the library data from file. Please try again")
 
     def show_books(self):
         if not self.books:
@@ -33,15 +67,19 @@ class Book():
         if not self.is_borrowed:
             self.is_borrowed = True
             print(f"You borrowed : {self.title}")
+            return True
         else:
             print(f"{self.title} is already borrowed")
+            return False
     
     def return_book(self):
         if self.is_borrowed:
             self.is_borrowed = False
             print(f"You returned: {self.title}")
+            return True
         else:
             print(f"{self.title} was not borrowed")
+            return False
 
 def library_system():
     library = Library()
@@ -70,7 +108,8 @@ def library_system():
             isbn = input("Enter ISBN to borrow: ")
             for book in library.books:
                 if book.isbn == isbn:
-                    book.borrow()
+                    if book.borrow():
+                        library.save_book()
                     break
             else:
                 print("Book not found.")
@@ -78,7 +117,8 @@ def library_system():
             isbn = input("Enter the ISBN to return")
             for book in library.books:
                 if book.isbn == isbn:
-                    book.return_book()
+                    if book.return_book():
+                        library.save_book()
                     break
             else:
                 print("Book not found")
